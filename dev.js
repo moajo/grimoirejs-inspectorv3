c701f837b8d3a919563eddadf3cf901788f62592
moajo <mimirosiasd@gmail.com>
2017-12-29 14:06:21 +0900

    introduction of ts+react+webpack

--- dev.js
+++
@@ -1,7 +0,0 @@
-
-function hoge() {
-  var backgroundPageConnection = chrome.runtime.connect({
-    name: "devtools-page"
-});
-backgroundPageConnection.postMessage({path: "aaaaaaaaakllllllllll"});
-}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    dev: './dev.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+    // rules: [
+    //   {
+    //     test: /\.js$/,
+    //     exclude: /node_modules/,
+    //     loader: 'babel-loader',
+    //     options: {
+    //       presets: ['react', 'es2015', 'react-hmre']
+    //     },
+    //   },
+    // ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public/',
+    port: 8080,
+    inline: true,
+    historyApiFallback: true,
+    clientLogLevel: "info",
+    stats: { colors: true }
+  }
+};
