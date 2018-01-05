ad24c108e8e5206a20d908a409ab7fab3a4543c6
Kakeru Ishii <i.kakeru@grimoire.gl>
2018-01-05 15:39:40 +0900

    Trying fix redux problem

--- src/view/redux/common/CommonAction.ts
+++ src/view/redux/common/CommonAction.ts
@@ -1,8 +1,13 @@
 import { Action } from "redux";
+import CommonActionType from "./CommonActionType";
 
-type CommonAction = ICommonExampleAction;
+type CommonAction = ICommonExampleAction | ICommonExampleAction2;
 export default CommonAction;
 
 export interface ICommonExampleAction extends Action {
+    type: CommonActionType.COMMON_EXAMPLE_ACTION;
+}
 
+export interface ICommonExampleAction2 extends Action {
+    type: CommonActionType.COMMON_EXAMPLE_ACTION2;
 }
--- src/view/redux/common/CommonEpic.ts
+++ src/view/redux/common/CommonEpic.ts
@@ -1,15 +1,21 @@
-import { ActionsObservable } from "redux-observable";
-import { ICommonExampleAction } from "./CommonAction";
+import { ActionsObservable, Epic } from "redux-observable";
+import CommonAction, { ICommonExampleAction, ICommonExampleAction2 } from "./CommonAction";
 import { IState } from "../State";
-import { Store, MiddlewareAPI } from "redux";
+import { Store, MiddlewareAPI, Action } from "redux";
 import CommonActionType from "./CommonActionType";
 import "rxjs";
+import { ICommonState } from "./CommonState";
+
+type CommonEpic<T extends Action, D = any> = Epic<T, IState, D>;
 
 
-export const sampleEpic = (action$: ActionsObservable<ICommonExampleAction>, store: MiddlewareAPI<IState>) =>
+export const sampleEpic: Epic<CommonAction, IState, any> = (action$: ActionsObservable<CommonAction>, store: MiddlewareAPI<IState>) =>
     action$.ofType(CommonActionType.COMMON_EXAMPLE_ACTION)
-        .mapTo({ type: "TEST" })
+        .mapTo({ type: "COMMON_EXAMPLE_ACTION2" } as ICommonExampleAction2)
+export const sampleEpic2 = (action$: ActionsObservable<CommonAction>, store: MiddlewareAPI<IState>) =>
+    action$.ofType(CommonActionType.COMMON_EXAMPLE_ACTION2)
+        .mapTo({ type: "COMMON_EXAMPLE_ACTION2" } as ICommonExampleAction2)
 // Write more epics...
 
 
-export default [sampleEpic];
+export default [sampleEpic, sampleEpic2];
